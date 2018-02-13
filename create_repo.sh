5cc509d6f99e967522cc1d3964b27a70aaeb45b8
Chris "Koying" Browet <cbro@semperpax.com>
2018-02-13 21:37:53 +0100

    ADD: Hue

--- create_repo.sh
+++ create_repo.sh
@@ -1,1 +1,1 @@
-./create_repository.py https://github.com/koying/webinterface.youtube-dl https://github.com/koying/script.module.youtube.dl https://github.com/koying/plugin.video.arteplussept
+./create_repository.py https://github.com/koying/webinterface.youtube-dl https://github.com/koying/script.module.youtube.dl https://github.com/koying/plugin.video.arteplussept https://github.com/koying/script.kodi.hue.ambilight.git#krypton
